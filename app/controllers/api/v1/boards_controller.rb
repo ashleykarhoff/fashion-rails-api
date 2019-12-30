bd59ce168f33c589b32e6255c28c3423e38321b5
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-30 12:36:37 -0500

    Create new board object and return the board object along with board items

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -3,4 +3,24 @@ class Api::V1::BoardsController < ApplicationController
         boards = Board.all 
         render json: boards, include: ['board_items']
     end
+
+    def new
+        
+    end
+
+    def create
+        board = Board.new(board_params)
+        if board.valid?
+            board.save
+            render json: board, include: ['board_items']
+        else
+            render json: {board_error: "There was an issue creating your board"}
+        end
+    end
+
+    private
+
+    def board_params
+        params.permit(:user_id, :title)
+    end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,11 +9,12 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        user = User.create(user_params)
+        user = User.new(user_params)
         if user.valid?
+            user.save
             render json: user
         else
-            render json: "Email is already taken"
+            render json: {error: "Email is already taken"}
         end
     end
 
